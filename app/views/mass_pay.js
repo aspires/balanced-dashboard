0613f84d7c83b4c0d0737843f5b755bed0b4db78
Austin Spires <austinspires@gmail.com>
2013-09-20 16:21:19 -0700

    initial scaffolding work

--- app/views/mass_pay.js
+++ app/views/mass_pay.js
@@ -1,8 +1,20 @@
 Balanced.MassPayModalView = Balanced.View.extend({
 
+        csvToArray: function(CSV){
+                var allTextLines = CSV.split(/\r\n|\n/);
+                var entries = allTextLines[0].split(',');
+                var lines = [];
+                var headings = entries.splice(0,record_num);
+                while (entries.length>0) {
+                    var tarr = [];
+                    for (var j=0; j<record_num; j++) {
+                        tarr.push(headings[j]+":"+entries.shift());
+                    }
+                    lines.push(tarr);
+                }
+                return lines;
+        },
         templateName: 'modals/mass_pay',
-
-        amount_dollars: 0,
 
         didInsertElement: function () {
                 this.get('controller').on('openMassPayModal', this, this.open);
@@ -12,39 +24,28 @@ Balanced.MassPayModalView = Balanced.View.extend({
                 this.get('controller').off('openMassPayModal', this, this.open);
         },
 
-        open: function () {
-                var credit = Balanced.Credit.create({uri: '/v1/credits'});
-                credit.set('bank_account', Balanced.BankAccount.create({'type': 'checking'}));
-                this.set('model', credit);
-                this.set('amount_dollars', null);
-
-                $('#pay-seller').modal({
-                        manager: this.$()
-                });
-        },
-
         actions: {
                 save: function () {
-                        if (this.get('model.isSaving')) {
-                                return;
+                        var csvLength = csvToArray.length, element = null;
+                        for (var i = 0; i < length ; i++){
+                                element = csvToArray[i];
+                                var credit = Balanced.Credit.create({uri: '/v1/credits'});
+                                credit.set('name': element[name]);
+                                credit.set('bank_account', Balanced.BankAccount.create({
+                                                'type': element[account-type],
+                                                'account': element[account-number],
+                                                'routing': element[routing-number]
+                                }));
+                                credit.set('ammount': element[ammount]);
+                                credit.set('statement': element[statement]);
+                                credit.set('description': element[description]);
+
+                                credit.save()
                         }
-
-                        var self = this;
-                        var credit = this.get('model');
-
-                        var cents = null;
-                        try {
-                                cents = Balanced.Utils.dollarsToCents(this.get('amount_dollars'));
-                        } catch (error) {
-                                credit.set('validationErrors', {'amount': error});
-                                return;
-                        }
-                        credit.set('amount', cents);
-
-                        credit.save().then(function (credit) {
+                        function () {
                                 $('#pay-seller').modal('hide');
-                                self.get('controller').transitionToRoute('credits', credit);
-                        });
+                                        self.get('controller').transitionToRoute('credits');
+                        };
                 }
         }
 });
